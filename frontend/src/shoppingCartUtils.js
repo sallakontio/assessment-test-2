6e7579d9c5aee3be9018c9d24e426fe3e44be464
salla kontio <sallakatariinakontio@gmail.com>
2022-10-07 09:15:31 +0300

    Fixed second function and tested it, wokred OK

--- frontend/src/shoppingCartUtils.js
+++ frontend/src/shoppingCartUtils.js
@@ -13,7 +13,11 @@ function calculateTotal(items) {
 // eg. [{'name': 'Banana', 'price': 1}, {'name': 'Apple', 'price': 2}]
 // Should return the cart average.
 function calculateAvg(items) {
-  return 9999;
+  let total = 0;
+  for (var i = 0; i < items.length; i++) {
+    total = total + items[i].price;
+  }
+  return total / items.length;
 }
 
 export { calculateTotal, calculateAvg };
